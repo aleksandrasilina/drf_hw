c6028b28e4b4df8a3c834629891fdcb94687476c
Александра <aleksandrasilina@gmail.com>
2024-09-10 11:44:09 +0300

    change users\ models.py add Payment model

--- users/models.py
+++ users/models.py
@@ -1,7 +1,19 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from lms.models import Course, Lesson
+
 NULLABLE = {"blank": True, "null": True}
+PAYMENT_CHOICES = (
+    (
+        "CASH",
+        "наличные",
+    ),
+    (
+        "BANK_TRANSFER",
+        "банковский перевод",
+    ),
+)
 
 
 class User(AbstractUser):
@@ -38,3 +50,18 @@ class User(AbstractUser):
     class Meta:
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
+
+
+class Payment(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name="Пользователь", help_text="Укажите пользователя",
+        related_name="payments"
+    )
+    payment_date = models.DateField(auto_now_add=True, verbose_name="Дата оплаты")
+    paid_course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name="Оплаченный курс",
+                                    help_text="Укажите оплаченный курс", related_name="payments", **NULLABLE)
+    paid_lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, verbose_name="Оплаченный урок",
+                                    help_text="Укажите оплаченный урок", related_name="payments", **NULLABLE)
+    amount = models.PositiveIntegerField(verbose_name="Сумма оплаты", help_text="Укажите сумму оплаты")
+    payment_method = models.CharField(max_length=50, verbose_name="Метод оплаты",
+                                     help_text="Укажите метод оплаты", choices=PAYMENT_CHOICES)
