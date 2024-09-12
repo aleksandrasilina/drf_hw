ac8d3dce68b5156a96fa14d7b970a861c7896290
Александра <aleksandrasilina@gmail.com>
2024-09-12 23:26:45 +0300

    change users\urls.py add permission_classes

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework.permissions import AllowAny
 from rest_framework.routers import SimpleRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
@@ -15,8 +16,8 @@ urlpatterns = [
     path("payments/", PaymentListAPIView.as_view(), name="payments_list"),
     path("payments/create/", PaymentCreateAPIView.as_view(), name="payments_create"),
 
-    path('login/', TokenObtainPairView.as_view(), name='login'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/', TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(permission_classes=(AllowAny,)), name='token_refresh'),
 ]
 
 urlpatterns += router.urls
