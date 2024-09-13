3abd7aace9d52845f91e4b477838d07052cc900d
Александра <aleksandrasilina@gmail.com>
2024-09-13 18:24:07 +0300

    change users\ permissions.py add IsOwner, IsProfileOwner

--- users/permissions.py
+++ users/permissions.py
@@ -6,3 +6,24 @@ class IsModerator(BasePermission):
 
     def has_permission(self, request, view):
         return request.user.groups.filter(name="moderator").exists()
+
+    def has_object_permission(self, request, view, obj):
+        return False
+
+
+class IsOwner(BasePermission):
+    """Проверяет, является ли пользователь владельцем курса/урока."""
+
+    def has_object_permission(self, request, view, obj):
+        if request.user == obj.owner:
+            return True
+        return False
+
+
+class IsProfileOwner(BasePermission):
+    """Проверяет, является ли пользователь владельцем профиля."""
+
+    def has_object_permission(self, request, view, obj):
+        if request.user == obj:
+            return True
+        return False
