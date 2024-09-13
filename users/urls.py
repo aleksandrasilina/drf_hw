9f63eeb221dd07e0c76839832bcd120863414937
Александра <aleksandrasilina@gmail.com>
2024-09-13 18:26:13 +0300

    change users\ urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from rest_framework.permissions import AllowAny
 from rest_framework.routers import SimpleRouter
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView)
 
 from users.apps import UsersConfig
 from users.views import PaymentCreateAPIView, PaymentListAPIView, UserViewSet
@@ -15,9 +16,16 @@ router.register("", UserViewSet)
 urlpatterns = [
     path("payments/", PaymentListAPIView.as_view(), name="payments_list"),
     path("payments/create/", PaymentCreateAPIView.as_view(), name="payments_create"),
-
-    path('login/', TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name='login'),
-    path('token/refresh/', TokenRefreshView.as_view(permission_classes=(AllowAny,)), name='token_refresh'),
+    path(
+        "login/",
+        TokenObtainPairView.as_view(permission_classes=(AllowAny,)),
+        name="login",
+    ),
+    path(
+        "token/refresh/",
+        TokenRefreshView.as_view(permission_classes=(AllowAny,)),
+        name="token_refresh",
+    ),
 ]
 
 urlpatterns += router.urls
