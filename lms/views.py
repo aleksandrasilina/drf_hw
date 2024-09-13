8044e8782de1ac82ba0adf2243a0985a671ac83b
Александра <aleksandrasilina@gmail.com>
2024-09-13 10:07:37 +0300

    change lms\ views.py add IsModerator permission

--- lms/views.py
+++ lms/views.py
@@ -1,19 +1,28 @@
 from rest_framework.generics import (CreateAPIView, DestroyAPIView,
                                      ListAPIView, RetrieveAPIView,
                                      UpdateAPIView)
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from lms.models import Course, Lesson
 from lms.serializers import CourseSerializer, LessonSerializer
+from users.permissions import IsModerator
 
 
 class CourseViewSet(ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
+    def get_permissions(self):
+        if self.action in ["create", "destroy"]:
+            self.permission_classes = (~IsModerator, IsAuthenticated)
+            print("Permissions")
+        return super().get_permissions()
+
 
 class LessonCreateAPIView(CreateAPIView):
     serializer_class = LessonSerializer
+    permission_classes = (~IsModerator, IsAuthenticated)
 
 
 class LessonListAPIView(ListAPIView):
@@ -33,3 +42,4 @@ class LessonUpdateAPIView(UpdateAPIView):
 
 class LessonDestroyAPIView(DestroyAPIView):
     queryset = Lesson.objects.all()
+    permission_classes = (~IsModerator,)
