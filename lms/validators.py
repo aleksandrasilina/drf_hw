bf655e2de4cd4ddd899b5a10e90576d512665889
Александра <aleksandrasilina@gmail.com>
2024-09-19 12:28:55 +0300

    add lms\validators.py add VideoLinkValidator

---
+++ lms/validators.py
@@ -0,0 +1,17 @@
+import re
+
+from rest_framework.serializers import ValidationError
+
+
+class VideoLinkValidator:
+    def __init__(self, field):
+        self.field = field
+
+    def __call__(self, value):
+        pattern = "https://www.youtube.com/"
+        tmp_val = dict(value).get(self.field)
+        if not bool(re.match(pattern, tmp_val)):
+            raise ValidationError("Можно ссылаться только на видео с сайта 'youtube.com'")
+
+        # if not tmp_val.lower().startswith(pattern):
+        #     raise ValidationError("Можно ссылаться только на видео с сайта 'youtube.com'")
