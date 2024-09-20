ad4a15c303354dc9b734622694639998f54dece9
Александра <aleksandrasilina@gmail.com>
2024-09-20 13:31:16 +0300

    change lms\validators.py

--- lms/validators.py
+++ lms/validators.py
@@ -10,8 +10,9 @@ class VideoLinkValidator:
     def __call__(self, value):
         pattern = "https://www.youtube.com/"
         tmp_val = dict(value).get(self.field)
-        if not bool(re.match(pattern, tmp_val)):
-            raise ValidationError("Можно ссылаться только на видео с сайта 'youtube.com'")
+        if tmp_val:
+            if not bool(re.match(pattern, tmp_val)):
+                raise ValidationError("Можно ссылаться только на видео с сайта 'youtube.com'")
 
         # if not tmp_val.lower().startswith(pattern):
         #     raise ValidationError("Можно ссылаться только на видео с сайта 'youtube.com'")
