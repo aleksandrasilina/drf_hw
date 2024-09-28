662834efb4c05fa71b7c83a64a1a81f7dda58301
Александра <aleksandrasilina@gmail.com>
2024-09-28 23:11:59 +0300

    change users\tasks.py add check_last_login

--- users/tasks.py
+++ users/tasks.py
@@ -1,7 +1,10 @@
 from celery import shared_task
 from django.core.mail import send_mail
+from django.utils import timezone
+from datetime import timedelta
 
 from config import settings
+from users.models import User
 
 
 @shared_task
@@ -14,3 +17,19 @@ def send_subscription_info(email, course):
         from_email=settings.EMAIL_HOST_USER,
         recipient_list=[email],
     )
+
+
+@shared_task
+def check_last_login():
+    """Проверяет пользователей по дате последнего входа и блокирует их, если не заходили более месяца."""
+
+    users = User.objects.all()
+    for user in users:
+        if user.last_login:
+            if user.last_login < timezone.now() - timedelta(days=30):
+                user.is_active = False
+                user.save()
+        else:
+            if user.date_joined < timezone.now() - timedelta(days=30):
+                user.is_active = False
+                user.save()
