d969714ad5910caf5a75cc22e4c971065635b0c0
Александра <aleksandrasilina@gmail.com>
2024-09-28 23:12:26 +0300

    change settings.py add UPDATE_LAST_LOGIN, CELERY_BEAT_SCHEDULE

--- config/settings.py
+++ config/settings.py
@@ -118,6 +118,7 @@ AUTH_USER_MODEL = "users.User"
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    'UPDATE_LAST_LOGIN': True,
 }
 
 STRIPE_API_KEY = os.getenv("STRIPE_API_KEY")
@@ -130,6 +131,13 @@ CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL")
 CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND")
 
 CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
+
+CELERY_BEAT_SCHEDULE = {
+    "check_last_login": {
+        "task": "users.tasks.check_last_login",
+        "schedule": timedelta(days=1),
+    },
+}
 
 EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_PORT = os.getenv("EMAIL_PORT")
